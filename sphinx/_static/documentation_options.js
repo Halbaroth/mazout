e6693e81da0d1bf6d0de798e19727f6bea08a1dd
Halbaroth <tiky.halbaroth@gmail.com>
2022-12-25 11:45:58 +0000

    Deploying to gh-pages from  @ 036b9a1e6564983f8179f9f05185e65d73c56b48 🚀

---
+++ sphinx/_static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'master (036b9a1) (2022/12/25 11:45)',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false
+};
